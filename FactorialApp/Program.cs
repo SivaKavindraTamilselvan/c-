d77d724ebb065f340c4bdc48114c43703604c396
SivaKavindraTamilSelvan <sivakavindra@gmail.com>
2026-04-08 01:33:10 +0530

    Factorial Function Implemented in Program.cs

---
+++ FactorialApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Factorial{
+    class Hello{
+        static int Function(int a)
+        {
+            int b=1;
+            for (int i = a; i > 0; i--)
+            {
+                b*=i;
+            }
+            return b;
+        }
+        static void Main(string[] args){
+            Console.WriteLine("Enter the value");
+            int value = Convert.ToInt32(Console.ReadLine());
+            int c=Function(value);
+            Console.WriteLine($"The Factorial for {value} is {c}");
+        }
+    }
+}
