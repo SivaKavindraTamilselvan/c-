cd7531773e0cd7f8fb9c99d94fa87e3f98f3391c
SivaKavindraTamilSelvan <sivakavindra@gmail.com>
2026-04-08 01:39:12 +0530

    Added negative value validation and images

--- FactorialApp/Program.cs
+++ FactorialApp/Program.cs
@@ -1,19 +1,29 @@
-﻿namespace Factorial{
-    class Hello{
+﻿namespace Factorial
+{
+    class Hello
+    {
         static int Function(int a)
         {
-            int b=1;
+            int b = 1;
             for (int i = a; i > 0; i--)
             {
-                b*=i;
+                b *= i;
             }
             return b;
         }
-        static void Main(string[] args){
+        static void Main(string[] args)
+        {
             Console.WriteLine("Enter the value");
             int value = Convert.ToInt32(Console.ReadLine());
-            int c=Function(value);
-            Console.WriteLine($"The Factorial for {value} is {c}");
+            if (value >= 0)
+            {
+                int c = Function(value);
+                Console.WriteLine($"The Factorial for {value} is {c}");
+            }
+            else
+            {
+                Console.WriteLine($"Enter Only Postive Value.Negative value cannot be accepted");
+            }
         }
     }
 }
---
+++ FactorialApp/README.md
@@ -0,0 +1,16 @@
+## Objective
+
+Write a console application that calculates the factorial of a given number.
+
+## 📋 Requirements
+
+- [ ]  Read an integer from the user
+- [ ]  Validate the input (ensure it's a positive integer)
+- [ ]  Use loops (or recursion) to calculate the factorial
+- [ ]  Display the result in the console
+
+## Reference
+
+https://1drv.ms/o/c/4821c09fc343da3e/IgBlxy46IoW3QqA9KDAbyXh1AbKsxZdj8dW1sOAD6oRmj2k?e=Uv7Ezo
+
+## Screenshots
