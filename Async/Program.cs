f6f41c4dd0b7084501074ef70d7273c7cfa374b9
SivaKavindraTamilSelvan <sivakavindra@gmail.com>
2026-04-09 15:36:26 +0530

    readme file

---
+++ Async/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+public class ImageData
+{
+    public string? id { get; set; }
+    public string? author { get; set; }
+    public int width { get; set; }
+    public int height { get; set; }
+    public string? url { get; set; }
+    public string? download_url { get; set; }
+}
+class Program
+{
+    static async Task<List<ImageData>> FetchData()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            string url = "https://picsum.photos/v2/list?page=2&limit=10";
+
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            var data = JsonSerializer.Deserialize<List<ImageData>>(json);
+
+            return data ?? new List<ImageData>();
+        }
+    }
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Start the Task");
+
+        await Task.Delay(5000);
+        Console.WriteLine("Waited for 5 seconds");
+
+        var images = await FetchData();
+
+        foreach (var image in images)
+        {
+            Console.WriteLine($"Author: {image.author}");
+            Console.WriteLine($"URL: {image.download_url}");
+            Console.WriteLine("");
+        }
+
+    }
+}
---
+++ Async/README.md
@@ -0,0 +1,11 @@
+## Objective
+
+Develop a console application that performs multiple asynchronous operations concurrently.
+
+## 📋 Requirements
+
+- [ ]  Use `async` and `await` to fetch data from multiple simulated sources (e.g., using `Task.Delay` to mimic API calls)
+- [ ]  Aggregate the results once all tasks are complete
+- [ ]  Handle exceptions that may occur during asynchronous operations
+
+## Screenshots
