8c9926e8c5945eb177366c59695db9d2b7a8e4de
SivaKavindraTamilSelvan <sivakavindra@gmail.com>
2026-04-09 23:27:09 +0530

    Interface program in c# implemented

---
+++ Interface/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string Department { get; set; }
+
+    public virtual void Display()
+    {
+        Console.WriteLine(Name);
+        Console.WriteLine(Age);
+        Console.WriteLine(PhoneNumber);
+        Console.WriteLine(Department);
+    }
+
+}
+
+public class Student : Person
+{
+    public string rollNumber { get; set; }
+
+    public override void Display()
+    {
+        base.Display();
+        Console.WriteLine(rollNumber);
+    }
+}
+public class Teacher : Person
+{
+    public string Subject { get; set; }
+
+    public override void Display()
+    {
+        base.Display();
+        Console.WriteLine(Subject);
+    }
+}
+
+
+public interface College<T>
+{
+    void Add(List<T> list, T item);
+    void Update(List<T> list, T old_item, T item);
+    void Delete(List<T> list, T item);
+    void Display(List<T> list);
+}
+
+public class CollegeService<T> : College<T> where T : Person
+{
+    public void Add(List<T> list, T item)
+    {
+        Console.WriteLine("Student Details Added");
+        list.Add(item);
+        Console.WriteLine();
+    }
+
+    public void Update(List<T> list, T old_item, T item)
+    {
+        Console.WriteLine("Student Updation Happening");
+        int index = list.IndexOf(old_item);
+
+        if (index != -1)
+        {
+            list[index] = item;
+        }
+        Console.WriteLine();
+    }
+
+    public void Delete(List<T> list, T item)
+    {
+        Console.WriteLine("Deletion Happening");
+        list.Remove(item);
+        Console.WriteLine();
+    }
+
+    public void Display(List<T> list)
+    {
+        foreach (var item in list)
+        {
+            item.Display();
+            Console.WriteLine();
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Student> students = new List<Student>();
+        List<Teacher> teachers = new List<Teacher>();
+
+        CollegeService<Student> studentService = new CollegeService<Student>();
+        CollegeService<Teacher> teacherService = new CollegeService<Teacher>();
+
+        Student s1 = new Student { Name = "Siva Kavindra", Age = 21, Department = "CSD", PhoneNumber = "9442378188", rollNumber = "22CDR096" };
+
+        studentService.Add(students, s1);
+
+        Teacher t1 = new Teacher { Name = "Kavindra", Age = 41, Department = "CSD", Subject = "Web", PhoneNumber = "9489698188" };
+
+        teacherService.Add(teachers, t1);
+
+        Student s2 = new Student { Name = "Siva", Age = 21, Department = "CSD", PhoneNumber = "9442378188", rollNumber = "22CDR096" };
+
+        studentService.Add(students, s2);
+
+        studentService.Update(students, s1, s2);
+
+        studentService.Display(students);
+
+        studentService.Delete(students, s2);
+
+        studentService.Display(students);
+    }
+}
---
+++ Interface/README.md
@@ -0,0 +1,14 @@
+## Objective
+
+Implement a generic in-memory repository to perform CRUD operations.
+
+## 📋 Requirements
+
+- [ ]  Define an interface (e.g., `IRepository<T>`) with methods like `Add`, `Get`, `Update`, and `Delete`
+- [ ]  Create a generic class that implements this interface
+- [ ]  Use type constraints if necessary (e.g., `where T : class`)
+- [ ]  Write a simple console UI to demonstrate the repository with a sample entity (e.g., `Product` or `Student`)
+
+
+## screenshots
+
