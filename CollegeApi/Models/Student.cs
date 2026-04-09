a10941f0e7af3edffe4315468ae01f7bc424692b
SivaKavindraTamilSelvan <sivakavindra@gmail.com>
2026-04-10 01:35:56 +0530

    Student Model Added

--- CollegeApi/Models/Student.cs
+++ CollegeApi/Models/Student.cs
@@ -1,7 +1,14 @@
-public class Student
+namespace CollegeApi.Models
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public int Age { get; set; }
-    public string Department { get; set; }
+    public class Student
+    {
+        public int Id {get;set;}
+        public string Name {get;set;} = "";
+
+        public int Age {get;set;}
+
+        public string Department {get;set;} = "";
+        
+
+    }
 }
