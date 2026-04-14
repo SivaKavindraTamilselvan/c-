25c9e54ea933703e14db2315a64f9a9303f5645b
SivaKavindraTamilSelvan <sivakavindra@gmail.com>
2026-04-15 01:03:15 +0530

    Implemented the task and requirenments

--- FactorialApp/obj/Debug/net10.0/FactorialApp.AssemblyInfo.cs
+++ FactorialApp/obj/Debug/net10.0/FactorialApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FactorialApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a10941f0e7af3edffe4315468ae01f7bc424692b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7520ebb12cf40ea1736f12058c4fa4a6d2b671a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FactorialApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FactorialApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
